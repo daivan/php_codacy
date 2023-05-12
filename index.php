df7510cffec20a4734edc08ae1fb0e77761ba590
daivan <daivan@gmail.com>
2023-05-12 14:49:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,22 @@
 
 # I love php
 
-echo "I really do love php";
+echo "I really dOnt like bread and php";
 
 
 function iLoveBread($hej) {
   return "Do you really love it?";
 }
 
+
+// This is the truth!
+  
 echo iLoveBread('does it really matter');
 
+class Peter {
+  
+  public funcution i_love_my_methods() {
+   return [];
+  }
+}
 exit;
